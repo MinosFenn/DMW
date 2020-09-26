db4791628afea75af9f6fb138042f2c7d869c0f3
MinosFenn <45685449+MinosFenn@users.noreply.github.com>
2020-09-26 17:14:53 +0200

    video 10 header menu setup

---
+++ Desktop/gatsby-wordpress/wordpress-gatsby/src/components/Footer.js
@@ -0,0 +1,14 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+
+const Footer = () => {
+  return (
+    <div>
+      Footer
+    </div>
+  )
+}
+
+export default Footer
---
+++ Desktop/gatsby-wordpress/wordpress-gatsby/src/components/Header.js
@@ -0,0 +1,14 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+
+const Header = () => {
+  return (
+    <div>
+      Header
+    </div>
+  )
+}
+
+export default Header
